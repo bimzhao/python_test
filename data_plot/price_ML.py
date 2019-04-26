18b7197c37b7324688234e4cac3427b71003e662
bzhao <zhaobi@ymail.com>
2019-04-26 23:16:05 +0100

    linear regression

--- data_plot/price_ML.py
+++ data_plot/price_ML.py
@@ -71,6 +71,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 #plot
 valid['Predictions'] = 0
 valid['Predictions'] = preds
+#print(valid)
 
 fig = plt.figure(figsize=(16,8))
 ax = fig.add_subplot(111)
@@ -78,7 +79,8 @@ ax.plot(train['Date'], train['Close']) #need add train[...]
 ax.plot(valid['Date'], valid[['Close', 'Predictions']]) #same above
 ax.set_xlim(datetime.date(2013,6,1), datetime.date(2018,12,1))
 
-plt.show()
+#plt.show()
+################################################################################################
 
 #setting index as date values
 df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
@@ -86,6 +88,8 @@ df.index = df['Date']
 
 #sorting
 data = df.sort_index(ascending=True, axis=0)
+#print(data)
+#print(df)
 
 #creating a separate dataset
 new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
@@ -94,12 +98,54 @@ for i in range(0,len(data)):
     new_data['Date'][i] = data['Date'][i]
     new_data['Close'][i] = data['Close'][i]
 
+print(new_data)
+
 #create features
 from fastai.structured import  add_datepart
 add_datepart(new_data, 'Date')
 new_data.drop('Elapsed', axis=1, inplace=True)  #elapsed will be the time stamp
+print(new_data)
+
+new_data['mon_fri'] = 0
+for i in range(0,len(new_data)):
+    if (new_data['Dayofweek'][i] == 0 or new_data['Dayofweek'][i] == 4):
+        new_data['mon_fri'][i] = 1
+    else:
+        new_data['mon_fri'][i] = 0
+
+#split into train and validation
+train = new_data[:987]
+valid = new_data[987:]
+
+x_train = train.drop('Close', axis=1)
+y_train = train['Close']
+x_valid = valid.drop('Close', axis=1)
+y_valid = valid['Close']
+
+#implement linear regression
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(x_train,y_train)
 
 
+#make predictions and find the rmse
+preds = model.predict(x_valid)
+rms=np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)),2)))
+print(rms)
 
+#plot
+valid['Predictions'] = 0
+valid['Predictions'] = preds
 
+valid.index = new_data[987:].index
+train.index = new_data[:987].index
+print(valid[['Year', 'Month','Day']])
+
+fig = plt.figure(figsize=(16,8))
+ax = fig.add_subplot(111)
+ax.plot(train['Close'])
+ax.plot(valid[['Close', 'Predictions']])
+#ax.set_xlim(datetime.date(2013,6,1), datetime.date(2018,12,1))
+
+plt.show()
 
